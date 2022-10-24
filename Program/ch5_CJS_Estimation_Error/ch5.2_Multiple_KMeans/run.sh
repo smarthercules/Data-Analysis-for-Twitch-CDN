b43a2f108ff82f70eab88156decf9aef7f78a6b4
Cheng <32033691+smarthercules@users.noreply.github.com>
2022-10-24 10:19:41 +0200

    period: 0 -> 1

--- Program/ch5_CJS_Estimation_Error/ch5.2_Multiple_KMeans/run.sh
+++ Program/ch5_CJS_Estimation_Error/ch5.2_Multiple_KMeans/run.sh
@@ -5,8 +5,8 @@ do
 	for loop in $files
 	do
 		echo "start: n_clusters_$n_cluster, label_$loop"
-		python kmeans_phase1.py 0 $n_cluster $loop
+		python kmeans_phase1.py 1 $n_cluster $loop
 		Rscript CJS.R ./p1_capture_history/n_cluster_$n_cluster/label_$loop.txt ./p1_capture_history/n_cluster_$n_cluster/group_label_$loop.txt > ./R_capture_probability/n_cluster_$n_cluster/r_$loop.txt
-		python cheng_kmeans_phase2.py 0 $n_cluster $loop
+		python cheng_kmeans_phase2.py 1 $n_cluster $loop
 		done
 done
